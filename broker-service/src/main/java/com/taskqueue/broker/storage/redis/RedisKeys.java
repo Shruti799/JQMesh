5167f130647a17297f9239eff560d6f6d3697501
Shruti799 <shrutijwal999@gmail.com>
2026-08-01 00:38:04 +0530

    Implemented redis key generation class

--- broker-service/src/main/java/com/taskqueue/broker/storage/redis/RedisKeys.java
+++ broker-service/src/main/java/com/taskqueue/broker/storage/redis/RedisKeys.java
@@ -1,5 +1,36 @@
 package com.taskqueue.broker.storage.redis;
 
+import java.util.UUID;
+
 public class RedisKeys {
+    private static final String QUEUE_PREFIX = "queue:";
+    private static final String TASK_PREFIX = "task:";
+    private static final String LEASE_PREFIX = "lease:";
+    private static final String IDEMPOTENCY_PREFIX = "idempotency:";
+    private static final String DLQ_PREFIX = "dlq:";
+
+    private RedisKeys() {
+        // Prevent instantiation
+    }
+
+    public static String queue(String queueName) {
+        return QUEUE_PREFIX + queueName;
+    }
+
+     public static String task(UUID taskId) {
+        return TASK_PREFIX + taskId;
+    }
+
+    public static String lease(String leaseId) {
+        return LEASE_PREFIX + leaseId;
+    }
+
+    public static String idempotency(String idempotencyKey) {
+        return IDEMPOTENCY_PREFIX + idempotencyKey;
+    }
+
+    public static String deadLetterQueue(String queueName) {
+        return DLQ_PREFIX + queueName;
+    }
     
 }
